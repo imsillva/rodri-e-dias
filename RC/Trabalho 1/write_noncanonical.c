3e0bf97e86764a61268b3f5eadbcccd16291e5d2
Ze0nBD <goncalo05realidade@gmail.com>
2026-03-10 14:10:39 +0000

    Add files via upload

---
+++ RC/Trabalho 1/write_noncanonical.c
@@ -0,0 +1,184 @@
+// Write to serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include <signal.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+
+#define BUF_SIZE 5
+
+volatile int STOP = FALSE;
+
+
+void alarmHandler(int signal)
+{
+    alarmEnabled = FALSE;
+    alarmCount++;
+
+    printf("Alarm #%d received\n", alarmCount);
+}
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+    int ler;
+    int bytes;
+    unsigned char buf2[BUF_SIZE] = {0};
+
+    struct sigaction act = {0};
+    act.sa_handler = &alarmHandler;
+    if (sigaction(SIGALRM, &act, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
+    }
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing, and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] =5; // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    printf("Alarm configured\n");
+
+    // Create string to send
+    unsigned char buf[BUF_SIZE] = {0};
+    unsigned char A = 0x03;
+    unsigned char C = 0x03;
+    unsigned char BCC = A^C;
+    unsigned char F = 0x7E;
+    buf[0] = F;
+    buf[1] = A;
+    buf[2] = C;
+    buf[3] = BCC;
+    buf[4] = F;
+
+    //for (int i = 0; i < BUF_SIZE; i++)
+    //{
+      //  buf[i] = 'a' + i % 26;
+    //}
+
+    // In non-canonical mode, '\n' does not end the writing.
+    // Test this condition by placing a '\n' in the middle of the buffer.
+    // The whole buffer must be sent even with the '\n'.
+    //buf[5] = '\n';
+
+
+    while (alarmCount <4)
+    {   
+        bytes = write(fd, buf, BUF_SIZE);
+
+        if (alarmEnabled == FALSE)
+        {   
+            alarmEnabled = TRUE;
+            alarm(3);
+             // Set alarm to be triggered in 3s
+        }
+        
+        
+        ler = read(fd,buf2,BUF_SIZE);
+
+        if (ler != -1){
+            printf("Bytes lidos\n");
+            for ( int i = 0 ; i < ler;i++){
+                printf("var = 0x%02X\n", buf2[i]);
+            }
+            break;
+        }
+        
+    
+    }
+
+    printf("Ending program\n");
+
+    // fflush(stdout);
+    
+
+    // Wait until all bytes have been written to the serial port
+    sleep(1);
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
